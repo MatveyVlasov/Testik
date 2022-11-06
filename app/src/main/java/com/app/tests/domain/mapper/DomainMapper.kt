589af53cb6565acdfab512eac2e8689f6fd4d96d
MatveyVlasov <vlasovmatvey007@gmail.com>
2022-11-06 20:10:20 +0300

    Refactor username validation

--- app/src/main/java/com/app/tests/domain/mapper/DomainMapper.kt
+++ app/src/main/java/com/app/tests/domain/mapper/DomainMapper.kt
@@ -4,13 +4,13 @@ import com.app.tests.data.model.LoginDto
 import com.app.tests.data.model.RegistrationDto
 import com.app.tests.domain.model.LoginModel
 import com.app.tests.domain.model.RegistrationModel
-import com.app.tests.util.removeWhiteSpaces
+import com.app.tests.util.toUsername
 
 fun RegistrationModel.toDto() =
     RegistrationDto(
         email = email,
         password = password,
-        username = username.removeWhiteSpaces().lowercase()
+        username = username.toUsername()
     )
 
 fun LoginModel.toDto() =
--- app/src/main/java/com/app/tests/util/Utils.kt
+++ app/src/main/java/com/app/tests/util/Utils.kt
@@ -2,6 +2,8 @@ package com.app.tests.util
 
 fun Int?.orZero() = this ?: 0
 
-fun String.removeWhiteSpaces() = filter { !it.isWhitespace() }
+fun Char.isDigitOrLatinLowercase() = this.isDigit() || this in 'a'..'z'
 
-fun String.isUsername() = all { c -> c.isDigit() || c in 'a'..'z' }
+fun String.isUsername() = all { it.isDigitOrLatinLowercase() }
+
+fun String.toUsername() = filter { c -> c.isDigitOrLatinLowercase() }.lowercase()
