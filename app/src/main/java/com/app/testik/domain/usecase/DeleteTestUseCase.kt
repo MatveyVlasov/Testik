29468f1c4abedb2417dfad63c9c854e907642cbf
MatveyVlasov <vlasovmatvey007@gmail.com>
2022-11-23 10:49:25 +0300

    Add test image deletion

--- app/src/main/java/com/app/testik/domain/usecase/DeleteTestUseCase.kt
+++ app/src/main/java/com/app/testik/domain/usecase/DeleteTestUseCase.kt
@@ -1,18 +1,23 @@
 package com.app.testik.domain.usecase
 
 import com.app.testik.domain.model.Result
+import com.app.testik.domain.model.onSuccess
+import com.app.testik.domain.repository.StorageRepository
 import com.app.testik.domain.repository.TestRepository
 import com.app.testik.domain.util.ResultWrapper
 import com.app.testik.domain.util.ResultWrapperImpl
 import javax.inject.Inject
 
 class DeleteTestUseCase @Inject constructor(
-    private val testRepository: TestRepository
+    private val testRepository: TestRepository,
+    private val storageRepository: StorageRepository
 ) : ResultWrapper by ResultWrapperImpl() {
 
     suspend operator fun invoke(testId: String): Result<Unit> =
         wrap(
             block = { testRepository.deleteTest(testId) },
             mapper = { }
-        )
+        ).onSuccess {
+            storageRepository.deleteTestImage(testId)
+        }
 }
