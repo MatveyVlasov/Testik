e50afa5fbdfbe920454cd4a5e5ef143c9ff6b79b
MatveyVlasov <vlasovmatvey007@gmail.com>
2023-01-11 11:00:38 +0300

    Add app check

--- app/src/main/java/com/app/testik/App.kt
+++ app/src/main/java/com/app/testik/App.kt
@@ -2,6 +2,9 @@ package com.app.testik
 
 import android.app.Application
 import com.app.testik.util.MediaLoader
+import com.google.firebase.FirebaseApp
+import com.google.firebase.appcheck.FirebaseAppCheck
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory
 import com.yanzhenjie.album.Album
 import com.yanzhenjie.album.AlbumConfig
 import dagger.hilt.android.HiltAndroidApp
@@ -21,5 +24,11 @@ class App : Application() {
                 .build()
         )
 
+        FirebaseApp.initializeApp(this)
+        val firebaseAppCheck = FirebaseAppCheck.getInstance()
+        firebaseAppCheck.installAppCheckProviderFactory(
+            // PlayIntegrityAppCheckProviderFactory.getInstance()
+            SafetyNetAppCheckProviderFactory.getInstance()
+        )
     }
 }
